780b8aec7d76c31cffea71d4cac9ad8ff1317e89
gEt-rIgHt-jR <abhidnya.pro@gmail.com>
2017-03-06 16:58:33 +0530

    minor changes

--- app/src/main/java/com/aceshub/portal/database/model/Timetable.java
+++ app/src/main/java/com/aceshub/portal/database/model/Timetable.java
@@ -8,55 +8,54 @@ import java.sql.Time;
 
 public class Timetable {
     //int Fid;
-    String subCode,subTitle,day;
-    Time start,end;
+    private String subCode, subTitle, day;
+    private Time start, end;
 
     public Timetable() {
     }
 
     //Setter
 
-    public void setSubCode(String subCode) {
-        this.subCode = subCode;
-    }
-
-    public void setSubTitle(String subTitle) {
-        this.subTitle = subTitle;
-    }
-
-    public void setDay(String day) {
-        this.day = day;
-    }
-
-    public void setStart(Time start) {
-        this.start = start;
-    }
-
-    public void setEnd(Time end) {
-        this.end = end;
-    }
-
-
-    // getter
-
-
     public String getSubCode() {
         return subCode;
+    }
+
+    public void setSubCode(String subCode) {
+        this.subCode = subCode;
     }
 
     public String getSubTitle() {
         return subTitle;
     }
 
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
+    }
+
     public String getDay() {
         return day;
+    }
+
+
+    // getter
+
+    public void setDay(String day) {
+        this.day = day;
     }
 
     public Time getStart() {
         return start;
     }
 
+    public void setStart(Time start) {
+        this.start = start;
+    }
+
     public Time getEnd() {
         return end;
+    }
+
+    public void setEnd(Time end) {
+        this.end = end;
     }
 }
